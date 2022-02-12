226327b2c86e1aae5d9908fa3db054090209f8b1
NEX31RY <vaibhav.dandage@pb.com>
2022-02-12 05:19:54 -0500

    second commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,13 +7,27 @@ import { ProjectService } from '../project.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor(private service:ProjectService) { }
-
+  
+  constructor(private service:ProjectService) {}
+  products:Product[] = [
+    {title:'ASUS Laptop - 5GB RAM',price:'$999',
+    imageUrl:"https://res.cloudinary.com/dxfq3iotg/image/upload/v1571750967/Ecommerce/ef192a21ec96.jpg",
+    shippingCharges:'Free Shipping'}
+  ]
   ngOnInit(): void {
     this.service.getProducts().subscribe((data)=>{
-      console.log(data)
-    })
+      console.log(data);
+      this.products= <any>data;
+      console.log(this.products);
+    });
   }
 
 }
+
+
+class Product {  
+  title : string;  
+  price : string;  
+  imageUrl : string;  
+  shippingCharges : string;  
+} 
